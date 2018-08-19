1792add738d389e10c758c99f93f4c127d29955c
Takako Sato <tea-ko@live.jp>
2018-08-19 11:14:34 +0000

    ログイン機能を追加

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'bcrypt', '3.1.11'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :feeds
+
+  resources :feeds do
+    post :confirm, on: :collection
+  end
+  
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
